513bb4697e954b3ff4f6ce06e63303e2f8315523
Christian Stowers <cstowers77@gmail.com>
2021-03-15 20:11:52 -0600

    project start

---
+++ Shared/Find_The_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_The_WeatherApp.swift
+//  Shared
+//
+//  Created by Jeremy Christian Stowers on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Find_The_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
